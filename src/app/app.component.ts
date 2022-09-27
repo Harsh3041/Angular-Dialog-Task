82ec805f274a17b763fcade23baac64516e5f8f4
Harsh <harshsharma56384@gmail.com>
2022-09-27 13:47:11 +0530

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First_task';
+
+  //Properties used in Data Binding
+  input1:any;
+  newinput1:any;
+  input2:any;
+  newinput2:any;
+  input3:any;
+  newinput3:any;
+  isShown: boolean = false ;
+  myForm: FormGroup;
+  title = 'angular-material-dialog-app';
+
+
+  // Code for Displaying Dialog Box
+  @ViewChild('dialogRef')
+  dialogRef!: TemplateRef<any>;
+
+  constructor(public dialog: MatDialog) {
+    this.myForm = new FormGroup({
+      acc: new FormControl(''),
+
+    })
+   }
+
+
+  openTempDialog() {
+    const myTempDialog = this.dialog.open(this.dialogRef, {  });
+    myTempDialog.afterClosed().subscribe((res) => {
+    });
+  }
+
+  closeTempDialog(){
+    this.dialog.closeAll();
+  }
+
+  // Property helping in hiding and showing Accordion
+  // hidden by default
+
+
+  // Method helping in toggling the div in which Accordion is stored
+  toggleShow() {
+    if(this.input1.value == ''){
+      this.isShown = false;
+    }
+    else if(this.input2.value != '' && this.input3.value != ''){
+      this.isShown = true;
+    }
+    else{
+      this.isShown =true;
+    }
+   }
+
+  // Method which handles the change of property value on Binding
+  getChange(){
+   this.newinput1 = this.input1;
+   this.newinput2 = this.input2;
+   this.newinput3 = this.input3;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatExpansionModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
